1c164072c1d5a5d995073fdeac7d6860ccbe3857
Nick Ng <ngyewch@gmail.com>
2023-07-05 12:21:39 +0800

    chore: updated build scripts

--- build-release.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-goreleaser build --rm-dist
-
-echo "Output distribution found in dist"
--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-goreleaser build --snapshot --rm-dist
-
-echo "Output distribution found in dist"
